2e67b90ce7229fe71e725ec17779e1183696c988
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-09-12 22:28:07 +0530

    Create Edge_class.java

---
+++ Edge_class.java
@@ -0,0 +1,10 @@
+static class Edge{
+        int src;
+        int dest;
+        int wt;
+        public Edge(int s,int d,int w){
+            this.src=s;
+            this.dest=d;
+            this.wt=w;
+        }
+    }
